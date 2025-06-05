46e5d6f841d162cd4c9066e21bd75ab38866ad44
Illia Kryvoviaz <illiakryvoviaz@gmail.com>
2025-06-05 22:14:58 +0200

    Refactored our methods (total repair and partial repair)

--- humancompatible/repair/group_blind_repair.py
+++ humancompatible/repair/group_blind_repair.py
@@ -65,123 +65,197 @@ def baseline(C, e, px, ptx, K):
     return gamma_classic[2 * K]
 
 # our method | total repair
-def total_repair(C,e,px,ptx,V,K):
-    bin=len(px)
-    bbm1=np.matrix(np.ones(bin)).T
-    I=np.where(~(V==0))[0].tolist()
-    xi=np.exp(-C/e)
-    gamma_dict=dict()
-    gamma_dict[0]=np.matrix(xi+1.0e-9)
-    gamma_dict[1]=np.matrix(np.diag((px/(gamma_dict[0] @ bbm1)).A1))@gamma_dict[0]
-    gamma_dict[2]=gamma_dict[1]@np.matrix(np.diag((ptx/(gamma_dict[1].T @ bbm1)).A1))
+def total_repair(C, e, px, ptx, V, K):
+    bin = len(px)
+    bbm1 = np.matrix(np.ones(bin)).T
+    I = np.where(~(V == 0))[0].tolist()
+    xi = np.exp(-C / e)
+
+    gamma_dict = {}
+    gamma_dict[0] = np.matrix(xi + 1.0e-9)
+    gamma_dict[1] = np.matrix(np.diag((px / (gamma_dict[0] @ bbm1)).A1)) @ gamma_dict[0]
+    gamma_dict[2] = gamma_dict[1] @ np.matrix(
+        np.diag((ptx / (gamma_dict[1].T @ bbm1)).A1)
+    )
+
     # step 3
-    J=np.where(~((gamma_dict[2].T @ V).A1 ==0))[0].tolist()
-    nu=np.zeros(bin)
-    gamma_dict[3]=np.copy(gamma_dict[2])
+    J = np.where(~((gamma_dict[2].T @ V).A1 == 0))[0].tolist()
+    gamma_dict[3] = np.copy(gamma_dict[2])
+
     for j in J:
-        fun = lambda z: sum(gamma_dict[2].item(i,j)*V.item(i)*np.exp(z*V.item(i)) for i in I)
-        dfun = lambda z: sum(gamma_dict[2].item(i,j)*(V.item(i))**2*np.exp(z*V.item(i)) for i in I)
-        nu = newton(fun,dfun,0,stepmax = 50,tol = 1.0e-9) 
+        fun = lambda z: sum(
+            gamma_dict[2].item(i, j) * V.item(i) * np.exp(z * V.item(i)) for i in I
+        )
+        dfun = lambda z: sum(
+            gamma_dict[2].item(i, j) * (V.item(i) ** 2) * np.exp(z * V.item(i)) for i in I
+        )
+        nu = newton(fun, dfun, 0, stepmax=50, tol=1.0e-9) 
         for i in I:
-            gamma_dict[3][i,j]=np.exp(nu*V.item(i))*gamma_dict[2].item(i,j)
-    gamma_dict[3]=np.matrix(gamma_dict[3])
+            gamma_dict[3][i, j] = np.exp(nu * V.item(i)) * gamma_dict[2].item(i, j)
+
+    gamma_dict[3] = np.matrix(gamma_dict[3])
 
     #=========================
-    L=3
-    q_dict=dict()
+    L = 3
+    q_dict = {}
+
     for loop in range(1,K):
-        if np.any(gamma_dict[(loop-1)*L+1]==0):
+        if np.any(gamma_dict[(loop - 1) * L + 1] == 0):
             break
-        tmp,q_dict[(loop-1)*L+1]=tmp_generator(gamma_dict,loop*L+1,q_dict,(loop-2)*L+1,L) #np.matrix(gamma_dict[3].A1*gamma_dict[0].A1/gamma_dict[1].A1)
-        gamma_dict[loop*L+1]=np.matrix(np.diag((px/(tmp @ bbm1)).A1))@tmp
-        if np.any(gamma_dict[(loop-1)*L+2]==0):
+
+        tmp, q_dict[(loop - 1) * L + 1] = tmp_generator(
+            gamma_dict, loop * L + 1, q_dict, (loop - 2) * L + 1, L
+        )
+        gamma_dict[loop * L + 1] = (
+            np.matrix(np.diag((px / (tmp @ bbm1)).A1)) @ tmp
+        )
+
+        if np.any(gamma_dict[(loop - 1) * L + 2] == 0):
             break
-        tmp,q_dict[(loop-1)*L+2]=tmp_generator(gamma_dict,loop*L+2,q_dict,(loop-2)*L+2,L)  #np.matrix(gamma_dict[4].A1*gamma_dict[1].A1/gamma_dict[2].A1)
-        gamma_dict[loop*L+2]=tmp@np.matrix(np.diag((ptx/(tmp.T @ bbm1)).A1))
+
+        tmp, q_dict[(loop - 1) * L + 2] = tmp_generator(
+            gamma_dict, loop * L + 2, q_dict, (loop - 2) * L + 2, L
+        )
+        gamma_dict[loop * L + 2] = tmp @ np.matrix(
+            np.diag((ptx / (tmp.T @ bbm1)).A1)
+        )
 
         # step 3
-        if np.any(gamma_dict[(loop-1)*L+3]==0):
+        if np.any(gamma_dict[(loop - 1) * L + 3] == 0):
             break
-        tmp,q_dict[(loop-1)*L+3]=tmp_generator(gamma_dict,loop*L+3,q_dict,(loop-2)*L+3,L)  #np.matrix(gamma_dict[5].A1*gamma_dict[2].A1/gamma_dict[3].A1)
-        J=np.where(~((abs(np.matrix(tmp).T @ V).A1)<=1.0e-9))[0].tolist()
-        gamma_dict[loop*L+3]=np.copy(tmp)
+
+        tmp, q_dict[(loop - 1) * L + 3] = tmp_generator(
+            gamma_dict, loop * L + 3, q_dict, (loop - 2) * L + 3, L
+        )
+        J = np.where(~((abs(np.matrix(tmp).T @ V).A1) <= 1.0e-9))[0].tolist()
+        gamma_dict[loop * L + 3] = np.copy(tmp)
+
         for j in J:
-            fun = lambda z: sum(tmp.item(i,j)*V.item(i)*np.exp(z*V.item(i)) for i in I)
-            dfun = lambda z: sum(tmp.item(i,j)*(V.item(i))**2*np.exp(z*V.item(i)) for i in I)
-            nu = newton(fun,dfun,0,stepmax = 50,tol = 1.0e-9) 
+            fun = lambda z: sum(
+                tmp.item(i, j) * V.item(i) * np.exp(z * V.item(i)) for i in I
+            )
+            dfun = lambda z: sum(
+                tmp.item(i, j) * (V.item(i) ** 2) * np.exp(z * V.item(i)) for i in I
+            )
+            nu = newton(fun, dfun, 0, stepmax=50, tol=1.0e-9)
             for i in I:
-                gamma_dict[loop*L+3][i,j]=np.exp(nu*V.item(i))*tmp.item(i,j)
-        gamma_dict[loop*L+3]=np.matrix(gamma_dict[loop*L+3])
-        # if sum(abs(gamma_dict[loop*L+3].T@V))<=1.0e-9:  #'tr violation:'
-        #         break;
-    #assess(bin,dist['x'],dist['t_x'],C,V,gamma_dict[K*L])
-    return gamma_dict[loop*L+3]
+                gamma_dict[loop * L + 3][i, j] = (
+                    np.exp(nu * V.item(i)) * tmp.item(i, j)
+                )
+
+        gamma_dict[loop * L + 3] = np.matrix(gamma_dict[loop * L + 3])
+
+    return gamma_dict[loop * L + 3]
 
 # our method | partial repair
-def partial_repair(C,e,px,ptx,V,theta_scale,K):
-    bin=len(px)
-    bbm1=np.matrix(np.ones(bin)).T
-    I=np.where(~(V==0))[0].tolist()
-    xi=np.exp(-C/e)
-    theta=bbm1*theta_scale
-    gamma_dict=dict()
-    gamma_dict[0]=np.matrix(xi+1.0e-9)
-    gamma_dict[1]=np.matrix(np.diag((px/(gamma_dict[0] @ bbm1)).A1))@gamma_dict[0]
-    gamma_dict[2]=gamma_dict[1]@np.matrix(np.diag((ptx/(gamma_dict[1].T @ bbm1)).A1))
+def partial_repair(C, e, px, ptx, V, theta_scale, K):
+    bin = len(px)
+    bbm1 = np.matrix(np.ones(bin)).T
+    I = np.where(~(V == 0))[0].tolist()
+    xi = np.exp(-C / e)
+    theta = bbm1 * theta_scale
+
+    gamma_dict = {}
+    gamma_dict[0] = np.matrix(xi + 1.0e-9)
+    gamma_dict[1] = np.matrix(np.diag((px / (gamma_dict[0] @ bbm1)).A1)) @ gamma_dict[0]
+    gamma_dict[2] = gamma_dict[1] @ np.matrix(
+        np.diag((ptx / (gamma_dict[1].T @ bbm1)).A1)
+    )
+
     # step 3
-    Jplus=np.where(~((gamma_dict[2].T @ V).A1 <=theta.A1))[0].tolist()
-    Jminus=np.where(~((gamma_dict[2].T @ V).A1>=-theta.A1))[0].tolist()
-    gamma_dict[3]=np.copy(gamma_dict[2])
+    Jplus = np.where(~((gamma_dict[2].T @ V).A1 <= theta.A1))[0].tolist()
+    Jminus = np.where(~((gamma_dict[2].T @ V).A1 >= -theta.A1))[0].tolist()
+    gamma_dict[3] = np.copy(gamma_dict[2])
+
     for j in Jplus:
-        fun = lambda z: sum(gamma_dict[2].item(i,j)*V.item(i)*np.exp(-z*V.item(i)) for i in I)-theta.item(j)
-        dfun = lambda z: -sum(gamma_dict[2].item(i,j)*(V.item(i))**2*np.exp(-z*V.item(i)) for i in I)
-        nu = newton(fun,dfun,0,stepmax = 50,tol = 1.0e-9)
+        fun = lambda z: sum(
+            gamma_dict[2].item(i, j) * V.item(i) * np.exp(-z * V.item(i)) for i in I
+        ) - theta.item(j)
+        dfun = lambda z: -sum(
+            gamma_dict[2].item(i, j) * (V.item(i) ** 2) * np.exp(-z * V.item(i)) for i in I
+        )
+        nu = newton(fun, dfun, 0, stepmax=50, tol=1.0e-9)
         for i in I:
-            gamma_dict[3][i,j]=np.exp(-nu*V.item(i))*gamma_dict[2].item(i,j)
+            gamma_dict[3][i, j] = np.exp(-nu * V.item(i)) * gamma_dict[2].item(i, j)
+    
     for j in Jminus:
-        fun = lambda z: sum(gamma_dict[2].item(i,j)*V.item(i)*np.exp(-z*V.item(i)) for i in I)+theta.item(j)
-        dfun = lambda z: -sum(gamma_dict[2].item(i,j)*(V.item(i))**2*np.exp(-z*V.item(i)) for i in I)
-        nu = newton(fun,dfun,0,stepmax = 50,tol = 1.0e-9)
+        fun = lambda z: sum(
+            gamma_dict[2].item(i, j) * V.item(i) * np.exp(-z * V.item(i)) for i in I
+        ) + theta.item(j)
+        dfun = lambda z: -sum(
+            gamma_dict[2].item(i, j) * (V.item(i) ** 2) * np.exp(-z * V.item(i)) for i in I
+        )
+        nu = newton(fun, dfun, 0, stepmax=50, tol=1.0e-9)
         for i in I:
-            gamma_dict[3][i,j]=np.exp(-nu*V.item(i))*gamma_dict[2].item(i,j)
-    gamma_dict[3]=np.matrix(gamma_dict[3])
+            gamma_dict[3][i, j] = np.exp(-nu * V.item(i)) * gamma_dict[2].item(i, j)
+
+    gamma_dict[3] = np.matrix(gamma_dict[3])
 
     #=========================
-    L=3
-    q_dict=dict()
-    for loop in range(1,K):
-        if np.any(gamma_dict[(loop-1)*L+1]==0):
+    L = 3
+    q_dict = {}
+
+    for loop in range(1, K):
+        if np.any(gamma_dict[(loop - 1) * L + 1] == 0):
             break
-        tmp,q_dict[(loop-1)*L+1]=tmp_generator(gamma_dict,loop*L+1,q_dict,(loop-2)*L+1,L) #np.matrix(gamma_dict[3].A1*gamma_dict[0].A1/gamma_dict[1].A1)
-        gamma_dict[loop*L+1]=np.matrix(np.diag((px/(tmp @ bbm1)).A1))@tmp
-        if np.any(gamma_dict[(loop-1)*L+2]==0):
+
+        tmp, q_dict[(loop - 1) * L + 1] = tmp_generator(
+            gamma_dict, loop * L + 1, q_dict, (loop - 2) * L + 1, L
+        )
+        gamma_dict[loop * L + 1] = (
+            np.matrix(np.diag((px / (tmp @ bbm1)).A1)) @ tmp
+        )
+
+        if np.any(gamma_dict[(loop - 1) * L + 2] == 0):
             break
-        tmp,q_dict[(loop-1)*L+2]=tmp_generator(gamma_dict,loop*L+2,q_dict,(loop-2)*L+2,L)  #np.matrix(gamma_dict[4].A1*gamma_dict[1].A1/gamma_dict[2].A1)
-        gamma_dict[loop*L+2]=tmp@np.matrix(np.diag((ptx/(tmp.T @ bbm1)).A1))
+
+        tmp, q_dict[(loop - 1) * L + 2] = tmp_generator(
+            gamma_dict, loop * L + 2, q_dict, (loop - 2) * L + 2, L
+        )
+        gamma_dict[loop * L + 2] = tmp @ np.matrix(
+            np.diag((ptx / (tmp.T @ bbm1)).A1)
+        )
 
         # step 3
-        if np.any(gamma_dict[(loop-1)*L+3]==0):
+        if np.any(gamma_dict[(loop - 1) * L + 3] == 0):
             break
-        tmp,q_dict[(loop-1)*L+3]=tmp_generator(gamma_dict,loop*L+3,q_dict,(loop-2)*L+3,L)  #np.matrix(gamma_dict[5].A1*gamma_dict[2].A1/gamma_dict[3].A1)
-        Jplus=np.where(~((np.matrix(tmp).T @ V).A1 <=theta.A1))[0].tolist()
-        Jminus=np.where(~((np.matrix(tmp).T @ V).A1>=-theta.A1))[0].tolist()
-        gamma_dict[loop*L+3]=np.copy(tmp)
+        
+        tmp, q_dict[(loop - 1) * L + 3] = tmp_generator(
+            gamma_dict, loop * L + 3, q_dict, (loop - 2) * L + 3, L
+        )
+        Jplus = np.where(~((np.matrix(tmp).T @ V).A1 <= theta.A1))[0].tolist()
+        Jminus = np.where(~((np.matrix(tmp).T @ V).A1 >= -theta.A1))[0].tolist()
+        gamma_dict[loop * L + 3] = np.copy(tmp)
+
         for j in Jplus:
-            fun = lambda z: sum(tmp.item(i,j)*V.item(i)*np.exp(-z*V.item(i)) for i in I)-theta.item(j)
-            dfun = lambda z: -sum(tmp.item(i,j)*(V.item(i))**2*np.exp(-z*V.item(i)) for i in I)
-            nu = newton(fun,dfun,0,stepmax = 50,tol = 1.0e-9) 
+            fun = lambda z: sum(
+                tmp.item(i, j) * V.item(i) * np.exp(-z * V.item(i)) for i in I
+            ) - theta.item(j)
+            dfun = lambda z: -sum(
+                tmp.item(i, j) * (V.item(i) ** 2) * np.exp(-z * V.item(i)) for i in I
+            )
+            nu = newton(fun, dfun, 0, stepmax=50, tol=1.0e-9)
             for i in I:
-                gamma_dict[loop*L+3][i,j]=np.exp(-nu*V.item(i))*tmp.item(i,j)
+                gamma_dict[loop * L + 3][i, j] = (
+                    np.exp(-nu * V.item(i)) * tmp.item(i, j)
+                )
+        
         for j in Jminus:
-            fun = lambda z: sum(tmp.item(i,j)*V.item(i)*np.exp(-z*V.item(i)) for i in I)+theta.item(j)
-            dfun = lambda z: -sum(tmp.item(i,j)*(V.item(i))**2*np.exp(-z*V.item(i)) for i in I)
-            nu = newton(fun,dfun,0,stepmax = 50,tol = 1.0e-9) 
+            fun = lambda z: sum(
+                tmp.item(i, j) * V.item(i) * np.exp(-z * V.item(i)) for i in I
+            ) + theta.item(j)
+            dfun = lambda z: -sum(
+                tmp.item(i, j) * (V.item(i) ** 2) * np.exp(-z * V.item(i)) for i in I
+            )
+            nu = newton(fun, dfun, 0, stepmax=50, tol=1.0e-9)
             for i in I:
-                gamma_dict[loop*L+3][i,j]=np.exp(-nu*V.item(i))*tmp.item(i,j)
-        gamma_dict[loop*L+3]=np.matrix(gamma_dict[loop*L+3])
-        # if sum(abs(gamma_dict[loop*L+3].T@V))<=1.0e-9:  #'tr violation:'
-        #     break;
-    return gamma_dict[loop*L+3]
+                gamma_dict[loop * L + 3][i, j] = (
+                    np.exp(-nu * V.item(i)) * tmp.item(i, j)
+                )
+
+        gamma_dict[loop * L + 3] = np.matrix(gamma_dict[loop * L + 3])
+
+    return gamma_dict[loop * L + 3]
 
 def empirical_distribution(sub,x_range):
     bin=len(x_range)
